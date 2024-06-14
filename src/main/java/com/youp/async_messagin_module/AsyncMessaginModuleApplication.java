db9836e599280c81be2ae88810f7af89b9f00627
SeungyoupBaek <bsy8220@naver.com>
2024-06-14 14:23:16 +0900

    First commit

---
+++ src/main/java/com/youp/async_messagin_module/AsyncMessaginModuleApplication.java
@@ -0,0 +1,13 @@
+package com.youp.async_messagin_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncMessaginModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncMessaginModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youp/async_messagin_module/AsyncMessaginModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youp.async_messagin_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncMessaginModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
