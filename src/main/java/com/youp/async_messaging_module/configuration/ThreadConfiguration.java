5d1add7b5b2684cb22a1a1a37a39e6951b2d611d
SeungyoupBaek <bsy8220@naver.com>
2024-06-14 15:13:35 +0900

    [fix] Modify thread configuration

--- src/main/java/com/youp/async_messaging_module/configuration/ThreadConfiguration.java
+++ src/main/java/com/youp/async_messaging_module/configuration/ThreadConfiguration.java
@@ -1,17 +1,14 @@
 package com.youp.async_messaging_module.configuration;
 
-import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
-@EnableAsync
 public class ThreadConfiguration {
 
     @Bean(name = "taskExecutor")
-    public Executor taskExecutor() {
+    public ThreadPoolTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(5);
         executor.setMaxPoolSize(10);
